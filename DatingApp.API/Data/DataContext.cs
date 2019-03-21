10926bbe768f095929fdb98501e68d5ae78d54b7
Jun Il Park <frmestcst@ordinarymystic.charter.com>
2019-03-21 12:45:36 -0700

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<Value> Values { get; set; } // Values is going to be the table name in the DB.
+    }
+}
